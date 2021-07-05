45721e8cf6ad6651f06eced003dff17710296d7c
nj-eka <njeka0108@gmail.com>
2021-07-05 22:01:00 +0300

    lsn1/v0.0.1

--- lsn1/task3.go
+++ lsn1/task3.go
@@ -6,19 +6,26 @@ import (
 	"os"
 )
 
+func createFile(fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Printf("Failed to create file: %s\n", fileName)
+	}
+	defer file.Close()
+}
+
 func CreateFiles(dir string, countFiles int) error {
-	if err := os.Mkdir(dir, 0700); err != nil { //os.ModeDir
+	if err := os.Mkdir(dir, os.ModeDir|os.ModePerm); err != nil {
 		fmt.Println("Failed to make directory :", err)
 		return err
 	}
 	for i := 0; i < countFiles; i++ {
-		fileName := fmt.Sprintf("%s/file_%d", dir, i) //dir + "/file_" + strconv.Itoa(i)
-		fo, _ := os.Create(fileName)
-		defer fo.Close()
+		fileName := fmt.Sprintf("%s/file_%d", dir, i)
+		createFile(fileName)
 	}
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Printf("can't read dir %s: %s\n", dir, err)
+		fmt.Printf("can't read dir %s: %s\n", dir, err) // too many open files
 	}
 	fmt.Printf("Created %d files.\n", len(files))
 	fmt.Println("Cleaning up")
